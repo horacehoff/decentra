20e4f405e9fe4fba2a9ef22172251e81519bc1fa
Just_a_Mango <68585097+just-a-mango@users.noreply.github.com>
2022-02-24 20:03:15 +0100

    Add files via upload

---
+++ decentra_client.py
@@ -0,0 +1,60 @@
+import socket, os
+ENC_KEY = "--__o__xxxxxx1969xo_uhfthfs8ef4s68ef680xxxxx--"
+def encrypt(data, key): # encrypt(no explanation either)
+    import random
+    result = ''
+    message = ''
+    characters_in_order = [chr(x) for x in range(16, 128)]
+    message = data
+    r_seed = key
+    random.seed(r_seed)
+    shuffled_list = [chr(x) for x in range(16, 128)]
+    random.shuffle(shuffled_list)
+    for i in range(0, len(message)):
+        result += shuffled_list[characters_in_order.index(message[i])]
+    return result
+
+def decrypt(data, key): #decrypt(no explanation)
+    import random
+    result = ''
+    message = ''
+    characters_in_order = [chr(x) for x in range(16, 128)]
+    message = data
+    r_seed = key
+    random.seed(r_seed)
+    shuffled_list = [chr(x) for x in range(16, 128)]
+    random.shuffle(shuffled_list)
+    for i in range(0, len(message)):
+        result += characters_in_order[shuffled_list.index(message[i])]
+    return result
+
+def parse_db(data):
+    all_lists = data.replace("'","").split("!")
+    articles = all_lists[0].strip('][').split(', ')
+    articles_descriptions = all_lists[1].strip('][').split(', ')
+    articles_titles = all_lists[2].strip('][').split(', ')
+    print(articles, articles_descriptions, articles_titles)
+    with open("db_decentra.txt","w+") as f:
+        f.write(str(articles)+"\n"+str(articles_descriptions)+"\n"+str(articles_titles)+"\n")
+
+
+def decrypt_data(data):
+    return decrypt(decrypt(data.decode('utf-8'), "get fucked"), ENC_KEY)
+
+def encrypt_data(data):
+    return bytes(encrypt(encrypt(data.encode('utf-8'), "get fucked"), ENC_KEY))
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 10969        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    while True:
+        if os.path.isfile("./db_decentra.txt"):
+            s.connect((HOST, PORT))
+            data = s.recv(4096)
+            data = decrypt_data(data)
+            print('Received', data)
+            parse_db(data)
+        else:
+            s.connect((HOST, PORT))
+            s.sendall(encrypt_data("request_db"))
---
+++ decentra_server.py
@@ -0,0 +1,98 @@
+import socket
+from datetime import datetime
+
+ENC_KEY = "--__o__xxxxxx1969xo_uhfthfs8ef4s68ef680xxxxx--"
+nodes = []
+
+with open("decentra_server.log","w+") as f:
+    f.write("Decentra Server Log")
+
+def encrypt(data, key): # encrypt(no explanation either)
+    import random
+    result = ''
+    message = ''
+    characters_in_order = [chr(x) for x in range(16, 128)]
+    message = data
+    r_seed = key
+    random.seed(r_seed)
+    shuffled_list = [chr(x) for x in range(16, 128)]
+    random.shuffle(shuffled_list)
+    for i in range(0, len(message)):
+        result += shuffled_list[characters_in_order.index(message[i])]
+    return result
+
+def decrypt(data, key): #decrypt(no explanation)
+    import random
+    result = ''
+    message = ''
+    characters_in_order = [chr(x) for x in range(16, 128)]
+    message = data
+    r_seed = key
+    random.seed(r_seed)
+    shuffled_list = [chr(x) for x in range(16, 128)]
+    random.shuffle(shuffled_list)
+    for i in range(0, len(message)):
+        result += characters_in_order[shuffled_list.index(message[i])]
+    return result
+
+def add_to_log(data):
+    with open("decentra_server.log","a") as f:
+        now = datetime.now()
+        current_time = now.strftime("%Y/%m/%d/%H:%M:%S")
+        f.write("\n("+current_time+")  -  "+data)
+        print("\n("+current_time+")  -  "+data)
+
+class log():
+    def info(data):
+        add_to_log("INFO: "+data)
+    def warning(data):
+        add_to_log("WARNING: "+data)
+    def critical(data):
+        add_to_log("CRITICAL: "+data)
+        
+
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 10969        # Port to listen on (non-privileged ports are > 1023)
+articles = ["heyya","ayo"]
+articles_descriptions = ["ayo bitch"]
+articles_titles = ["noice"]
+
+def send_db():
+    all_articles = str(articles)+"!"+str(articles_descriptions)+"!"+str(articles_titles)
+    all_articles = encrypt(all_articles, ENC_KEY)
+    all_articles = encrypt(all_articles, "get fucked")
+    return bytes(all_articles, 'utf-8')
+
+def decrypt_data(data):
+    return decrypt(decrypt(data.decode('utf-8'), "get fucked"), ENC_KEY)
+
+def encrypt_data(data):
+    return bytes(encrypt(encrypt(data.encode('utf-8'), "get fucked"), ENC_KEY))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            if "request_db" in decrypt(decrypt(s.recv(4096).decode('utf-8'), "get fucked"), ENC_KEY):
+                    import random
+                    try:
+                        selected_node = random.choice(nodes)
+                        conn.sendall(encrypt(selected_node, ENC_KEY))
+                        log.info("Sent node adress("+selected_node+") to requesting node -> "+str(addr))
+                    except:
+                        log.warning("Failed to send node adress("+selected_node+") to requesting node -> "+str(addr))
+            else:
+                log.info("New node -> "+str(addr))
+                nodes.append(addr)
+                print(nodes)
+                try:
+                    conn.sendall(send_db())
+                    log.info("Sent database to node -> "+str(addr))
+                    conn.close()
+                except Exception as e:
+                    print(e)
+                    log.warning("Failed to send database to node -> "+str(addr))
+                    pass
